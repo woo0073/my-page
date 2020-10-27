96f25cba83599ca7bfeabc4fd502ca7c211c8514
Jihye Woo <jihye.woo@stonybrook.edu>
2020-10-27 15:45:57 +0900

    Add all src for the main page
    - add source codes and data files for the main page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainPage from "./components/main_page";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
